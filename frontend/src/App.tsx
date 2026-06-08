62cb4a435864bdb4ffa576d3fd0cfe9e7394a5aa
Shayam Singh <shayamkumarsingh890@gmail.com>
2026-06-08 23:56:13 +0530

    restaurant , utils, orders services updated

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
-import ProtectedRoute from "./components/protectedRote";
+import ProtectedRoute from "./components/protectedRoute";
 import PublicRoute from "./components/publicRoute";
 import SelectRole from "./pages/SelectRole";
 import Navbar from "./components/navbar";
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
-import { GoogleOAuthProvider } from '@react-oauth/google'
-import { AppProvider } from './context/AppContext.tsx'
-// import 'leaflet/dist/leaflet.css';
-// import { SocketProvider } from './context/SocketContext.tsx'
-
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import { AppProvider } from "./context/AppContext.tsx";
+import "leaflet/dist/leaflet.css";
+import { SocketProvider } from "./context/SocketContext.tsx";
 
 export const authService = "http://localhost:5000";
 export const restaurantService = "http://localhost:5001";
@@ -15,14 +14,14 @@ export const realtimeService = "http://localhost:5004";
 export const riderService = "http://localhost:5005";
 export const adminService = "http://localhost:5006";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId='574846917982-t0dkdkia7jrmshe8hs3vt3q9t6og1oph.apps.googleusercontent.com'>
+    <GoogleOAuthProvider clientId="596268404302-kktqpqnl8t8r3fc33rb4clslfj8t3msc.apps.googleusercontent.com">
       <AppProvider>
-        {/* <SocketProvider> */}
-           <App />
-        {/* </SocketProvider> */}
+        <SocketProvider>
+          <App />
+        </SocketProvider>
       </AppProvider>
     </GoogleOAuthProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
